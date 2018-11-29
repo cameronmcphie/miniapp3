b881c8bd0371f6a276937fb49f605913078dc8c4
cameronmcphie <quiksilver980@gmail.com>
2018-11-29 00:19:14 -0800

    working in it

--- app/src/main/java/edu/fullerton/ecs/cpsc411/miniapp3/CalculatorFragment.kt
+++ app/src/main/java/edu/fullerton/ecs/cpsc411/miniapp3/CalculatorFragment.kt
@@ -2,53 +2,19 @@ package edu.fullerton.ecs.cpsc411.miniapp3
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
-import kotlinx.android.synthetic.main.fragment_file_transfer_speed.*
-import java.text.DecimalFormat
+
 
 class CalculatorFragment : Fragment() {
 
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        fileValue.onChange()
-        speedValue.onChange()
 
         return inflater.inflate(R.layout.fragment_file_transfer_speed, container, false)
     }
 
-    private fun calcTransferSpeed(): Double {
-        val sizeString = fileValue.text.toString()
-        val speedString = speedValue.text.toString()
 
-        return if (speedString.trim { it <= ' ' }.isEmpty() || sizeString.trim { it <= ' ' }.isEmpty()) {
-            0.0
-        } else {
-            var sizeDouble: Double = sizeString.toDouble()
-            var speedDouble: Double = speedString.toDouble()
 
-            // Size in bits
-            sizeDouble *= Math.pow(2.0, 20.0) * 8.0
-
-            // Speed in bits per sec
-            speedDouble *= Math.pow(10.0, 6.0)
-
-            return sizeDouble / speedDouble
-        }
-    }
-
-    private fun EditText.onChange() {
-        this.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {}
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                val transferTime = calcTransferSpeed()
-                val oneDecPlace = DecimalFormat("#.#")
-                transferSpeedValue.text = oneDecPlace.format(transferTime)
-            }
-        })
-    }
 }
--- app/src/main/java/edu/fullerton/ecs/cpsc411/miniapp3/FileTransferSpeed.kt
+++ app/src/main/java/edu/fullerton/ecs/cpsc411/miniapp3/FileTransferSpeed.kt
@@ -7,7 +7,9 @@ import android.text.TextWatcher
 import android.widget.EditText
 import java.text.DecimalFormat
 import kotlinx.android.synthetic.main.fragment_file_transfer_speed.*
+import kotlinx.android.synthetic.main.activity_file_transfer_speed.*
 import android.support.v4.app.FragmentManager
+import android.view.View
 
 class FileTransferSpeed : AppCompatActivity() {
 
@@ -18,12 +20,12 @@ class FileTransferSpeed : AppCompatActivity() {
         val fragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
 
-       val fragment = CalculatorFragment()
+        val fragment = CalculatorFragment()
         fragmentTransaction.add(R.id.main_activity, fragment)
         fragmentTransaction.commit()
 
-        fileValue.onChange()
-        speedValue.onChange()
+//        fileValue.onChange()
+//        speedValue.onChange()
 
     }
 
